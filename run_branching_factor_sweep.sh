cc8e4d96a6da516db025ff92c89133496623b4f9
dkhimey <khimeydiana@gmail.com>
2026-05-27 12:04:58 +0000

    run theoretical recall simulations

---
+++ run_branching_factor_sweep.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+ 
+set -e
+ 
+declare -A EXPERIMENTS
+ 
+#EXPERIMENTS["bigann-random"]="
+#    --base-dir       /dataset/big-ann-benchmarks/data/bigann-random/cluster_history_bigann-100M-random_10000
+#    --partitions-dir /dataset/big-ann-benchmarks/data/bigann-random/cluster_history_bigann-100M-random_10000_10
+#    --runbook-path   /dataset/big-ann-benchmarks/data/bigann-random/runbook-bigann-100M-random.yaml
+#    --query-file     /dataset/big-ann-benchmarks/data/bigann-random/query.public.10K.u8bin
+#    --base-file      /dataset/big-ann-benchmarks/data/bigann-random/bigann-100M-random.u8bin
+#    --gt-dir         /dataset/big-ann-benchmarks/data/bigann-random/100000000/runbook-bigann-100M-random.yaml
+#"
+ 
+#EXPERIMENTS["bigann-clustered"]="
+#    --base-dir       /dataset/big-ann-benchmarks/data/bigann-clustered/cluster_history_bigann-100M-clustered_10000
+#    --partitions-dir /dataset/big-ann-benchmarks/data/bigann-clustered/cluster_history_bigann-100M-clustered_10000_10
+#    --runbook-path   /dataset/big-ann-benchmarks/data/bigann-clustered/runbook-bigann-100M.yaml
+#    --query-file     /dataset/big-ann-benchmarks/data/bigann-clustered/query.public.10K.u8bin
+#    --base-file      /dataset/big-ann-benchmarks/data/bigann-clustered/bigann-100M-clustered.u8bin
+#    --gt-dir         /dataset/big-ann-benchmarks/data/bigann-clustered/100000000/runbook-bigann-100M.yaml/
+#"
+ 
+#EXPERIMENTS["msturing-clustered"]="
+#    --base-dir       /dataset/big-ann-benchmarks/data/MSTuring-100M-clustered/cluster_history_msturing-100M-clustered_10000
+#    --partitions-dir /dataset/big-ann-benchmarks/data/MSTuring-100M-clustered/cluster_history_msturing-100M-clustered_10000_10
+#    --runbook-path   /dataset/big-ann-benchmarks/data/MSTuring-100M-clustered/msturing-100M-clustered_runbookfinal.yaml
+#    --query-file     /dataset/big-ann-benchmarks/data/MSTuring-100M-clustered/testQuery10K.fbin
+#    --base-file      /dataset/big-ann-benchmarks/data/MSTuring-100M-clustered/100M-msturing-clustered.fbin
+#    --gt-dir         /dataset/big-ann-benchmarks/data/MSTuring-100M-clustered/100000000/msturing-100M-clustered_runbookfinal.yaml/
+#"
+ 
+EXPERIMENTS["msturing-random"]="
+    --base-dir       /dataset/big-ann-benchmarks/data/MSTuring-100M-random/cluster_history_msturing-100M-random_10000
+    --partitions-dir /dataset/big-ann-benchmarks/data/MSTuring-100M-random/cluster_history_msturing-100M-random_10000_10/
+    --runbook-path   /dataset/big-ann-benchmarks/data/MSTuring-100M-random/runbook-msturing-100M-random.yaml
+    --query-file     /dataset/big-ann-benchmarks/data/MSTuring-100M-random/testQuery10K.fbin
+    --base-file      /dataset/big-ann-benchmarks/data/MSTuring-100M-random/msturing-100M-random.fbin
+    --gt-dir         /dataset/big-ann-benchmarks/data/MSTuring-100M-random/100000000/runbook-msturing-100M-random.yaml/
+"
+ 
+#EXPERIMENTS["msturing-shift"]="
+#    --base-dir       /dataset/big-ann-benchmarks/data/MSTuring-100M-shift/cluster_history_msturing-100M-shift_10000
+#    --partitions-dir /dataset/big-ann-benchmarks/data/MSTuring-100M-shift/cluster_history_msturing-100M-shift_10000_10/
+#    --runbook-path   /dataset/big-ann-benchmarks/data/MSTuring-100M-shift/msturing-100M-shift_runbookfinal.yaml
+#    --query-file     /dataset/big-ann-benchmarks/data/MSTuring-100M-shift/testQuery10K.fbin
+#    --base-file      /dataset/big-ann-benchmarks/data/MSTuring-100M-shift/100M-msturing-shift.fbin
+#    --gt-dir         /dataset/big-ann-benchmarks/data/MSTuring-100M-shift/100000000/msturing-100M-shift_runbookfinal.yaml/
+#"
+ 
+for experiment in "${!EXPERIMENTS[@]}"; do
+    for threshold in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
+        echo "========================================="
+        echo "Experiment: ${experiment}  threshold: ${threshold}"
+        echo "========================================="
+        sudo python -u compute_theoretical_recall_updated.py \
+            ${EXPERIMENTS[$experiment]} \
+            --mode BranchingFactor \
+            --threshold "$threshold"
+    done
+done
+ 
+echo "Done."
